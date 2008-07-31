f638f040066f5381b38ea65f08554fb42908b7f0
redwallhp <redwallhp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-31 16:16:25 +0000

    Uploaded trunk files for 1.0.0.
    git-svn-id: https://plugins.svn.wordpress.org/wp125/trunk@57320 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adminmenus.php
@@ -0,0 +1,277 @@
+<?php
+
+//Write Manage Menu
+function wp125_write_managemenu() {
+echo '<div class="wrap">
+<h2>Manage Ads</h2>';
+
+//Handle deactivations
+if ($_GET['wp125action'] == "deactivate") {
+$theid = $_GET['theid'];
+echo '<div id="message" class="updated fade"><p>Are you sure you want to deactivate the ad? <a href="admin.php?page=wp125/wp125.php&wp125action=deactivateconf&theid='.$theid.'">Yes</a> &nbsp; <a href="admin.php?page=wp125/wp125.php">No!</a></p></div>';
+}
+if ($_GET['wp125action'] == "deactivateconf") {
+$theid = $_GET['theid'];
+global $wpdb, $table_prefix;
+$adtable_name = $wpdb->prefix . "wp125_ads";
+$wpdb->query("UPDATE $adtable_name SET status = '0' WHERE id = '$theid'");
+echo '<div id="message" class="updated fade"><p>Ad deactivated.</p></div>';
+}
+
+//Handle REactivations
+if ($_GET['wp125action'] == "activate") {
+$theid = $_GET['theid'];
+echo '<div id="message" class="updated fade"><p>Are you sure you want to reactivate the ad? <a href="admin.php?page=wp125/wp125.php&showmanage=inactive&wp125action=activateconf&theid='.$theid.'">Yes</a> &nbsp; <a href="admin.php?page=wp125/wp125.php&showmanage=inactive">No!</a></p></div>';
+}
+if ($_GET['wp125action'] == "activateconf") {
+$theid = $_GET['theid'];
+global $wpdb, $table_prefix;
+$adtable_name = $wpdb->prefix . "wp125_ads";
+$wpdb->query("UPDATE $adtable_name SET status = '1' WHERE id = '$theid'");
+echo '<div id="message" class="updated fade"><p>Ad activated.</p></div>';
+}
+
+echo '<ul class="subsubsub">'; ?>
+<li><a href="admin.php?page=wp125/wp125.php"  <?php if ($_GET['showmanage'] != 'inactive') { echo 'class="current"'; } ?>>Active Ads</a> |</li><li><a href="admin.php?page=wp125/wp125.php&showmanage=inactive" <?php if ($_GET['showmanage'] == 'inactive') { echo 'class="current"'; } ?>>Inactive Ads</a></li>
+<?php echo '</ul>
+<table class="widefat">
+<thead><tr>
+<th scope="col">Slot</th>
+<th scope="col">Name</th>
+<th scope="col" class="num">Clicks</th>
+<th scope="col">Start Date</th>
+<th scope="col">End Date</th>
+<th scope="col"></th>
+<th scope="col"></th>
+</tr></thead>
+<tbody>';
+
+global $wpdb;
+$adtable_name = $wpdb->prefix . "wp125_ads";
+if ($_GET['showmanage'] == 'inactive') {
+$wp125db = $wpdb->get_results("SELECT * FROM $adtable_name WHERE status = '0' ORDER BY id DESC", OBJECT);
+} else {
+$wp125db = $wpdb->get_results("SELECT * FROM $adtable_name WHERE status != '0' ORDER BY id DESC", OBJECT);
+}
+if ($wp125db) {
+foreach ($wp125db as $wp125db){
+
+echo '<tr>';
+echo '<td>'.$wp125db->slot.'</td>';
+echo '<td><strong>'.$wp125db->name.'</strong></td>';
+if ($wp125db->clicks!='-1') { echo '<td class="num">'.$wp125db->clicks.'</td>'; } else { echo '<td class="num">N/A</td>'; }
+echo '<td>'.$wp125db->start_date.'</td>';
+echo '<td>'.$wp125db->end_date.'</td>';
+echo '<td><a href="admin.php?page=wp125_addedit&editad='.$wp125db->id.'">Edit</a></td>';
+if ($_GET['showmanage'] == 'inactive') {
+echo '<td><a href="admin.php?page=wp125/wp125.php&showmanage=inactive&wp125action=activate&theid='.$wp125db->id.'">Activate</a></td>';
+} else {
+echo '<td><a href="admin.php?page=wp125/wp125.php&wp125action=deactivate&theid='.$wp125db->id.'">Deactivate</a></td>';
+}
+echo '</tr>';
+
+}
+} else { echo '<tr> <td colspan="8">No ads found.</td> </tr>'; }
+
+echo '</tbody>
+</table>
+</div>';
+}
+
+function wp125_write_addeditmenu() {
+//DB Data
+global $wpdb;
+$settingtable_name = $wpdb->prefix . "wp125_settings";
+$adtable_name = $wpdb->prefix . "wp125_ads";
+// Retrieve settings
+$wp125settings = $wpdb->get_row("SELECT * FROM $settingtable_name WHERE ad_orientation != '' ", OBJECT);
+//If post is being edited, grab current info
+if ($_GET['editad']!='') {
+$theid = $_GET['editad'];
+$editingad = $wpdb->get_row("SELECT * FROM $adtable_name WHERE id = '$theid'", OBJECT);
+}
+?><div class="wrap">
+
+<?php
+if ($_POST['Submit']) {
+$post_editedad = $wpdb->escape($_POST['editedad']);
+$post_adname = $wpdb->escape($_POST['adname']);
+$post_adslot = $wpdb->escape($_POST['adslot']);
+$post_adtarget = $wpdb->escape($_POST['adtarget']);
+$post_adexp = $wpdb->escape($_POST['adexp']);
+$post_adexpmo = $wpdb->escape($_POST['adexp-mo']);
+$post_adexpday = $wpdb->escape($_POST['adexp-day']);
+$post_adexpyr = $wpdb->escape($_POST['adexp-yr']);
+$post_countclicks = $wpdb->escape($_POST['countclicks']);
+$post_adimage = $wpdb->escape($_POST['adimage']);
+if ($post_countclicks=='on') { $post_countclicks = '0'; } else { $post_countclicks = '-1'; }
+$today = date('m').'/'.date('d').'/'.date('Y');
+if ($post_adexp=='manual') { $theenddate = '00/00/0000'; }
+if ($post_adexp=='other') { $theenddate = $post_adexpmo.'/'.$post_adexpday.'/'.$post_adexpyr; }
+if ($post_adexp=='30') { $expiry = time() + 30 * 24 * 60 * 60; $expiry = strftime('%m/%d/%Y', $expiry); $theenddate = $expiry; }
+if ($post_adexp=='60') { $expiry = time() + 60 * 24 * 60 * 60; $expiry = strftime('%m/%d/%Y', $expiry); $theenddate = $expiry; }
+if ($post_adexp=='90') { $expiry = time() + 90 * 24 * 60 * 60; $expiry = strftime('%m/%d/%Y', $expiry); $theenddate = $expiry; }
+if ($post_adexp=='120') { $expiry = time() + 120 * 24 * 60 * 60; $expiry = strftime('%m/%d/%Y', $expiry); $theenddate = $expiry; }
+if ($post_editedad!='') { $theenddate = $post_adexpmo.'/'.$post_adexpday.'/'.$post_adexpyr; }
+if ($post_editedad=='') {
+$updatedb = "INSERT INTO $adtable_name (slot, name, start_date, end_date, clicks, status, target, image_url) VALUES ('$post_adslot', '$post_adname', '$today','$theenddate','$post_countclicks', '1', '$post_adtarget','$post_adimage')";
+$results = $wpdb->query($updatedb);
+echo '<div id="message" class="updated fade"><p>Ad &quot;'.$post_adname.'&quot; created.</p></div>';
+} else {
+$updatedb = "UPDATE $adtable_name SET slot = '$post_adslot', name = '$post_adname', end_date = '$theenddate', target = '$post_adtarget', image_url = '$post_adimage' WHERE id='$post_editedad'";
+$results = $wpdb->query($updatedb);
+echo '<div id="message" class="updated fade"><p>Ad &quot;'.$post_adname.'&quot; updated.</p></div>';
+}
+}
+if ($_POST['deletead']) {
+$post_editedad = $wpdb->escape($_POST['editedad']);
+echo '<div id="message" class="updated fade"><p>Do you really want to delete this ad record? This action cannot be undone. <a href="admin.php?page=wp125_addedit&deletead='.$post_editedad.'">Yes</a> &nbsp; <a href="admin.php?page=wp125_addedit&editad='.$post_editedad.'">No!</a></p></div>';
+}
+if ($_GET['deletead']!='') {
+$thead=$_GET['deletead'];
+$updatedb = "DELETE FROM $adtable_name WHERE id='$thead'";
+$results = $wpdb->query($updatedb);
+echo '<div id="message" class="updated fade"><p>Ad deleted.</p></div>';
+}
+?>
+
+<h2>Add/Edit Ads</h2>
+
+<form method="post" action="admin.php?page=wp125_addedit">
+<table class="form-table">
+
+<?php if ($_GET['editad']!='') { echo '<input name="editedad" type="hidden" value="'.$_GET['editad'].'" />'; } ?>
+
+<tr valign="top">
+<th scope="row">Name</th>
+<td><input name="adname" type="text" id="adname" value="<?php echo $editingad->name; ?>" size="40" /><br/>Whose ad is this?</td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Slot</th>
+<td><label for="adslot">
+<select name="adslot" id="adslot">
+<?php for ($count = 1; $count <= $wp125settings->num_slots; $count += 1) { ?>
+<option value="<?php echo $count; ?>" <?php if ($count == $editingad->slot) { echo 'selected="selected"'; } ?>>#<?php echo $count; ?></option>
+<?php } ?>
+</select></label>
+</td></tr>
+
+<tr valign="top">
+<th scope="row">Target URL</th>
+<td><input name="adtarget" type="text" id="adtarget" value="<?php if ($editingad->target!='') { echo $editingad->target; } else { echo 'http://'; } ?>" size="40" /><br/>Where should the ad link to?</td>
+</tr>
+
+<?php if ($_GET['editad']!='') {
+$enddate = $editingad->end_date;
+if ($enddate != '00/00/0000') {
+$enddate = strtotime($enddate);
+$endmonth = date('m', $enddate);
+$endday = date('d', $enddate);
+$endyear = date('Y', $enddate);
+} else { $endmonth='00'; $endday='00'; $endyear='0000'; }
+} ?>
+<tr valign="top">
+<th scope="row">Expiration</th>
+<td><label for="adexp">
+<?php if ($_GET['editad']=='') { ?><select name="adexp" id="adexp" onChange="isOtherDate(this.value)">
+<option value="manual">I'll remove it manually</option>
+<option selected="selected" value="30">30 Days</option>
+<option value="60">60 Days</option>
+<option value="90">90 Days</option>
+<option value="120">120 Days</option>
+<option value="other">Other</option>
+</select><?php } ?></label>
+ <span id="adexp-date">&nbsp;&nbsp; Month: <input type="text" name="adexp-mo" id="adexp-mo" size="2" value="<?php if ($endmonth!='') { echo $endmonth; } else { echo date('m'); } ?>" /> Day: <input type="text" name="adexp-day" id="adexp-day" size="2" value="<?php if ($endday!='') { echo $endday; } else {  echo date('d'); } ?>" /> Year: <input type="text" name="adexp-yr" id="adexp-yr" size="4" value="<?php if ($endyear!='') { echo $endyear; } else {  echo date('Y'); } ?>" /> <?php if ($_GET['editad']!='') { ?><br /> &nbsp;&nbsp; Use 00/00/0000 for manual removal.<?php } ?></span>
+</td></tr>
+
+<?php if ($_GET['editad']=='') { ?><script type="text/javascript">
+document.getElementById("adexp-date").style.display = "none";
+function isOtherDate(obj) {
+if (obj=="other") {
+document.getElementById("adexp-date").style.display = "inline";
+} else {
+document.getElementById("adexp-date").style.display = "none";
+}
+}
+</script><?php } ?>
+
+<?php if ($_GET['editad']=='') { ?>
+<tr valign="top">
+<th scope="row">Click Tracking</th>
+<td><input type="checkbox" name="countclicks" checked="checked" /> Count the number of times this ad is clicked</td>
+</tr>
+<?php } ?>
+
+<tr valign="top">
+<th scope="row">Ad Image</th>
+<td><input name="adimage" type="text" id="adimage" value="<?php if ($editingad->image_url!='') { echo $editingad->image_url; } else { echo 'http://'; } ?>" size="40" /></td>
+</tr>
+
+</table>
+<p class="submit"><input type="submit" name="Submit" value="Save Ad" /> &nbsp; <?php if ($_GET['editad']!='') { ?><input type="submit" name="deletead" value="Delete Ad" /><?php } ?></p>
+</form>
+</div><?php
+}
+
+function wp125_write_settingsmenu() {
+//DB Data
+global $wpdb;
+$settingtable_name = $wpdb->prefix . "wp125_settings";
+//Add settings, if submitted
+if ($_POST['issubmitted']=='yes') {
+$post_adorient = $wpdb->escape($_POST['adorient']);
+$post_numslots = $wpdb->escape($_POST['numads']);
+$post_adorder = $wpdb->escape($_POST['adorder']);
+$post_salespage = $wpdb->escape($_POST['salespage']);
+$updatedb = "UPDATE $settingtable_name SET ad_orientation='$post_adorient', num_slots='$post_numslots', ad_order='$post_adorder', buyad_url='$post_salespage' WHERE ad_orientation != ''";
+$results = $wpdb->query($updatedb);
+echo '<div id="message" class="updated fade"><p>Settings updated.</p></div>';
+}
+// Retrieve settings
+$wp125settings = $wpdb->get_row("SELECT * FROM $settingtable_name WHERE ad_orientation != '' ", OBJECT);
+?><div class="wrap">
+<h2>Settings</h2>
+<form method="post" action="admin.php?page=wp125_settings">
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row">Ad Orientation</th>
+<td><label for="adorient">
+<select name="adorient" id="adorient">
+<option <?php if ($wp125settings->ad_orientation=='1c') { echo 'selected="selected"'; } ?> value="1c">One Column</option>
+<option <?php if ($wp125settings->ad_orientation=='2c') { echo 'selected="selected"'; } ?> value="2c">Two Column</option>
+</select></label>
+<br/>How many columns should the ads be displayed in?
+</td></tr>
+
+<tr valign="top">
+<th scope="row">Number of Ad Slots</th>
+<td><input name="numads" type="text" id="numads" value="<?php echo $wp125settings->num_slots; ?>" size="2" /><br/>How many ads should be shown?</td>
+</tr>
+
+<tr valign="top">
+<th scope="row">Ad Order</th>
+<td><label for="adorder">
+<select name="adorder" id="adorder">
+<option selected="selected" value="normal" <?php if ($wp125settings->ad_order=='normal') { echo 'selected="selected"'; } ?>>Normal</option>
+<option value="random" <?php if ($wp125settings->ad_order=='random') { echo 'selected="selected"'; } ?>>Random</option>
+</select></label>
+</td></tr>
+
+<tr valign="top">
+<th scope="row">Ad Sales Page</th>
+<td><input name="salespage" type="text" id="salespage" value="<?php echo $wp125settings->buyad_url; ?>" size="50" /><br/>Do you have a page with statistics and prices? (Leave blank to disable)</td>
+</tr>
+
+</table>
+<input name="issubmitted" type="hidden" value="yes" />
+<p class="submit"><input type="submit" name="Submit" value="Save Changes" /></p>
+</form>
+<br/>
+<p>Your ads can be displayed using either the included widget, or by using the <strong>&lt;?php wp125_write_ads();  ?&gt;</strong> template tag. Also, you can display a single ad, without any formatting, using <strong>&lt;?php wp125_single_ad(<em>num</em>);  ?&gt;</strong>, where <em>num</em> is the number of the ad slot you wish to show. This is useful for cases where your theme prevents the default formatting from working properly, or where you wish to display your ads in an unforeseen manner.</p>
+</div><?php
+}
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== Plugin Name ===
+Contributors: redwall_hp
+Plugin URI: http://www.webmaster-source.com/wp125-ad-plugin-wordpress/
+Author URI: http://www.webmaster-source.com
+Donate link: http://www.webmaster-source.com/donate/
+Tags: ads, 125x125, management, advertisement
+Requires at least: 2.3
+Tested up to: 2.5
+Stable tag: 1.0.0
+
+
+Easy management of 125x125 ads on your blog.  Ads can be run for a specified number of days, and will automatically be taken down. Track clicks too.
+
+
+
+== Description ==
+
+If you've given up the low-paying and slightly obtrusive ad networks used by many new bloggers, in favor of selling ads directly, you may have been frustrated with the excessive time involved managing your ads. Not only do you have to find advertisers to sponsor your blog, you have to manually edit your template to put the ad in, and then head over to your favorite calendar app to set an alert to remind you when to take the ad down.
+
+Time consuming practices like those are a thing of the past. The WP125 plugin can help you manage your ads more efficiently, leaving you with more time to write new posts. The plugin adds a new "Ads" menu to the WordPress admin, featuring submenus for tweaking display settings and adding and removing ads.
+
+Features include:
+
+* One or two column ad display, and support through template tags to implement your own unconventional design.
+* Show as many ads as you want, and in either manual or random order
+* Keep track of how many times an ad is clicked
+* When creating a new ad, you don't have to calculate the end date yourself. Just input how many days you wish the ad to run for, and the correct date will be applied. The ad will be automatically taken down when the time comes.
+* When an ad run is over, the record is archived on the Inactive ads screen, so you can check on the final click count, or revive the ad for another run.
+
+
+
+== Installation ==
+
+1. Download and unzip the package.
+2. FTP the entire "wp125" directory to your /wp-content/plugins/ directory on your blog.
+3. Activate the plugin on the "Plugins" tab of the administartion panel.
+4. Either use the included "WP125: Ads" widget, or place the <?php wp125_write_ads(); ?> template tag where you wish your ads to appear.
+5. Go to the new "Ads" section of the WordPress admin, where you can tweak settings, such as the maximum number of ads to be shown at once (the default is 6), and how they should be displayed.
+
+
+
+== Upgrading ==
+
+You may upgrade the plugin via the automated system in WordPress 2.5 or greater, or "old-style" by downloading the new one and then
+1. Deactivating plugin
+2. Uploading the updated files
+3. Reactivating plugin
+
+
+
+== Frequently Asked Questions ==
+
+= What if I don't want to arrange my ads in one OR two columns? =
+If you want to arrange your ads in an unconventional manner, you can use the <?php wp125_single_ad(num); ?> template tag (replace "num" with the number of an ad slot). The tag will return one ad with minimal formatting (simply <a><img /></a>). You can use multiple instances of the tag in your template to set lay your ad slots out in whatever way you choose.
+
+= One of my ads hit it's expiration date. Where did it go? =
+When an ad's time duration is over, it disappears off your site, and is removed from the Active ads page in the WordPress admin. To access the record, just click the "Inactive" link on the Manage screen. The page should update to show all of your inactive ads.
+
+= What if I don't want an ad to be taken down automatically? =
+Just select "I'll remove it manually" for the expiration date when you go to create the ad.
+
+
+
+== Screenshots ==
+1. A few 125x125 ads, in two-column format.
+2. The Add/Edit ads screen.
+
+
+
+== Known Issues ==
+* If you have WP Super Cache installed on your blog, it may conflict with WP125's click tracking feature. To fix this, add "index.php" on a new line in the "Rejected URLs" field of the WP Super Cache options page. This will disable caching for yourblog.com/index.php. If someone goes to yourblog.com, they will still get the cached version, but since WP125's click tracker URLs look like "/index.php?adclick=1," they will avoid the cache.
+
+
+
+== Support ==
+If you're having a problem with the plugin, try posting on the official WordPress forum at http://wordpress.org/support/ (be sure to use the tag "WP125"!). I, or another user of the plugin, will hopefully be able to answer your questions. Or send me an email via the contact form on Webmaster-Source.com.
