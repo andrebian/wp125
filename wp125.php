7cde2dc4ea4fa8f0c52b6b37aafde05978e5653c
redwallhp <redwallhp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-08 23:40:09 +0000

    Localization support should be *pretty much* complete. French translation in place.
    git-svn-id: https://plugins.svn.wordpress.org/wp125/trunk@85316 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp125.php
+++ wp125.php
@@ -65,6 +65,7 @@ function wp125_write_ads_widget($args) {
 //Add the Admin Menus
 if (is_admin()) {
 	function wp125_add_admin_menu() {
+		load_plugin_textdomain('wp125');
 		add_menu_page("125x125 Ads", "Ads", MANAGEMENT_PERMISSION, __FILE__, "wp125_write_managemenu");
 		add_submenu_page(__FILE__, "Manage 125x125 Ads", "Manage", MANAGEMENT_PERMISSION, __FILE__, "wp125_write_managemenu");
 		add_submenu_page(__FILE__, "Add/Edit 125x125 Ads", "Add/Edit", MANAGEMENT_PERMISSION, 'wp125_addedit', "wp125_write_addeditmenu");
