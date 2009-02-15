15afa14e73d1abd5b57c0368fd6d4cd7d08d1069
redwallhp <redwallhp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-15 04:46:44 +0000

    Friendlifying the admin menus a little.
    git-svn-id: https://plugins.svn.wordpress.org/wp125/trunk@94681 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adminmenus.php
+++ adminmenus.php
@@ -1,5 +1,9 @@
 <?php
 
+wp_enqueue_script('jquery');
+wp_enqueue_script('thickbox');
+wp_enqueue_style('thickbox');
+
 //Write Manage Menu
 function wp125_write_managemenu() {
 echo '<div class="wrap">
@@ -75,7 +79,7 @@ echo '</tr>';
 
 echo '</tbody>
 </table>';
-echo '<br /><a href="'.wp125_get_plugin_dir('url').'/calendar.php"><img src="'.wp125_get_plugin_dir('url').'/ical.gif" alt="iCalendar" /></a>';
+echo '<br /><a href="'.wp125_get_plugin_dir('url').'/calendar.php" title="Subscribe with your calendaring software..."><img src="'.wp125_get_plugin_dir('url').'/ical.gif" alt="iCalendar" /></a>';
 wp125_admin_page_footer();
 echo '</div>';
 }
@@ -366,7 +370,7 @@ add_action('wp_dashboard_setup', 'wp125_dashboard_add_widget' );
 function wp125_admin_page_footer() {
 echo '<div style="margin-top:45px; font-size:0.87em;">';
 echo '<div style="float:right;"><a href="http://www.webmaster-source.com/donate/" title="Why should you donate a few dollars? Click to find out..."><img src="https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif" alt="Donate" /></a></div>';
-echo '<div><a href="'.wp125_get_plugin_dir('url').'/readme.txt">'.__('Documentation', 'wp125').'</a> | <a href="http://www.webmaster-source.com/wp125-ad-plugin-wordpress/">'.__('WP125 Homepage', 'wp125').'</a></div>';
+echo '<div><a href="'.wp125_get_plugin_dir('url').'/readme.txt?KeepThis=true&amp;TB_iframe=true&amp;height=450&amp;width=680" class="thickbox" title="Documentation">'.__('Documentation', 'wp125').'</a> | <a href="http://www.webmaster-source.com/wp125-ad-plugin-wordpress/">'.__('WP125 Homepage', 'wp125').'</a></div>';
 echo '</div>';
 }
 
